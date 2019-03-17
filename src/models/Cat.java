71c632ec7930b92febacae7de892d5eeed98ddb7
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2019-03-17 12:38:01 +0100

    Add the real cat attributes

--- src/models/Cat.java
+++ src/models/Cat.java
@@ -2,8 +2,9 @@ package models;
 
 public final class Cat {
     public static String[][] Values = new String[][] {
-        {"Hat", "Blue", "Yellow"},
-        {"Mouth", "Smile", "Sad"},
-        {"Color", "Black", "White"},
+        {"Glasses", "No Glasses", "Sun Glasses", "Classic Glasses"},
+        {"Hat", "No Hat", "Winter Hat", "Feather Hat", "Captain Hat"},
+        {"Mouth", "Neutral", "Happy", "Surprised"},
+        {"Skin", "Black", "White", "Orange", "Brown"},
     };
 }
